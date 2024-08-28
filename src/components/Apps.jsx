7e23d8604e393207189ac6ea6936d5d96d68a871
Tousif-react-coding <tousifa925@gmail.com>
2024-08-28 11:31:29 +0530

    update

--- src/components/Apps.jsx
+++ src/components/Apps.jsx
@@ -7,7 +7,7 @@ const Apps = () => {
 <div className="my-3 text-center">
   <img src="logo.svg" alt="logo"  style={{ width: "25%" }}/>
   <div className="">
-  <a href="http://localhost:5173/">Go To Home</a>
+  <a href="https://zerodha-frontend-l8l3.onrender.com">Go To Home</a>
   </div>
  
 </div>
